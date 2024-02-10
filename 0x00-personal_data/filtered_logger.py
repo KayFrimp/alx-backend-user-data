817cbdef77eda986a094afd5b4a582f4eb2bba6b
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-02-10 23:26:57 +0000

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -61,8 +61,8 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
         password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
         host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=getenv('PERSONAL_DATA_DB_NAME')
-    )
+        database=getenv('PERSONAL_DATA_DB_NAME'))
+
     return connector
 
 
