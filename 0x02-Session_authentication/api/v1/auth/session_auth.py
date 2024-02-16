ead7bcb7a1748f9f059cac1af1ad226340618846
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-02-16 02:16:00 +0000

    SessionAuth update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,20 @@
 """SessionAuth Class Module"""
 
 
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Class definition"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
